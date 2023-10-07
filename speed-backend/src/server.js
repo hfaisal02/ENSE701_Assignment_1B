4a92171576d741c611986d49ce3359c2d2113291
hfaisal02 <106284237+hfaisal02@users.noreply.github.com>
2023-10-07 14:37:14 +1300

    Frontend & Backend setup

---
+++ speed-backend/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 5000; // Choose a port for your server
+
+// Define your routes and APIs here
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
