9618c73c3268a3ccb0c12c67a3580c64e2ce61c3
hfaisal02 <106284237+hfaisal02@users.noreply.github.com>
2023-10-21 17:24:55 +1300

    removed semi colon

--- speed-frontend/pages/api/submit.tsx
+++ speed-frontend/pages/api/submit.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SubmitArticle from '../../src/app/SubmitArticle';
-; // Import the SubmitArticle component
+// Import the SubmitArticle component
 
 const SubmitPage: React.FC = () => {
   return (
