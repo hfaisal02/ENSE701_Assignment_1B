92e2bc96f8789c3e9613dd6b9560f5f98ca2ff69
Shahir Khairati <trf8284@autuni.ac.nz>
2023-10-23 21:02:16 +1300

    Update intro.tsx

--- speed-frontend/pages/intro.tsx
+++ speed-frontend/pages/intro.tsx
@@ -2,11 +2,30 @@ import React from 'react';
 import '../src/app/globals.css'; // Import the CSS file
 import Navbar from '../components/Navbar'; // Import the Navbar component
 const IndexPage: React.FC = () => {
-  return <div>
+  return (
+  <div style={styles.container as React.CSSProperties}>
+    <h1 style={styles.heading}>Intro to SPEED</h1> {/* New heading */}
+  <div>
     <Navbar/> {/*Navbar Component*/}
     <label>Hello brah</label>
-    
-    </div>;
-}
+    </div>
+    </div>
+    );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  },
+  heading: {
+    fontSize: '2rem',
+    color: '#ffffff',
+    marginBottom: '20px',
+  },
+};
 
 export default IndexPage;
