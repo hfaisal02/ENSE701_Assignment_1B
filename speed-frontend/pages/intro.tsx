981a64a4bdbe508856dbecffcf650a7d303c5593
Shahir Khairati <trf8284@autuni.ac.nz>
2023-10-23 20:54:04 +1300

    Create intro.tsx

---
+++ speed-frontend/pages/intro.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../src/app/globals.css'; // Import the CSS file
+import Navbar from '../components/Navbar'; // Import the Navbar component
+const IndexPage: React.FC = () => {
+  return <div>Hello brah</div>;
+}
+
+export default IndexPage;
