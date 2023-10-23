93bdb422766c1e198f8df05a582cf536c19d5267
Shahir Khairati <trf8284@autuni.ac.nz>
2023-10-23 20:58:30 +1300

    added navbar to intro to SPEED

--- speed-frontend/pages/intro.tsx
+++ speed-frontend/pages/intro.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import '../src/app/globals.css'; // Import the CSS file
 import Navbar from '../components/Navbar'; // Import the Navbar component
 const IndexPage: React.FC = () => {
-  return <div>Hello brah</div>;
+  return <div>
+    <Navbar/> {/*Navbar Component*/}
+    <label>Hello brah</label>
+    
+    </div>;
 }
 
 export default IndexPage;
